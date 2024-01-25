cd6e47534e68ee95742768d45a56ccbb4f871ff3
Andrew Holland <53604413+therealandrewholland@users.noreply.github.com>
2024-01-25 05:47:13 -0500

    Revert "Adds built-in IT Glue client ID generation"

--- compile_command.bat
+++ compile_command.bat
@@ -1,3 +1,3 @@
 python -m PyInstaller -F -i "icon.ico" --name="Find Client OneNote" "%~dp0Find Client OneNote.pyw" --onedir
 
-copy "icon.ico" "dist\Find Client OneNote\" & copy "transparent.ico" "dist\Find Client OneNote\" & copy "chromedriver.exe" "dist\Find Client OneNote\" & copy "get_itglue_ids.js" "dist\Find Client OneNote\" & copy "get_itglue_ids.py" "dist\Find Client OneNote\"
+copy "icon.ico" "dist\Find Client OneNote\" & copy "transparent.ico" "dist\Find Client OneNote\" & copy "IT Glue Client IDs_JAN2024.json" "dist\Find Client OneNote\"
